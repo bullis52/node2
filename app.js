e323e2b9e9d4ab77bf733330f3d66aa9fdd5257f
1 <tawers6103@gmail.com>
2021-10-04 13:12:37 +0300

    node

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
-
 const fs  = require('fs')
 const path = require('path')
 
-function gender(folderFrom,folderTO,gender) {
-    fs.readdir(path.join(__dirname,`${folderFrom}`), (err, files) => {
-        if (err) {
-            console.log(err);
-        }
-        files.forEach(file => {
-            fs.readFile(path.join(__dirname,`${folderFrom}`,file), (err, data) => {
-                if(err) {
-                    return;
-                }
-                const parseUser = JSON.parse(data.toString())
-                console.log(parseUser)
-                if (parseUser.gender === gender){
-                    fs.rename(path.join(`${folderFrom}`, file), path.join(`${folderTO}`, file), (err) => {
-                        console.log(err);
-                    })
-                }
+
+// const mkDir = path.join(__dirname,'users')
+// fs.mkdir ()
+const usersPath = path.join(__dirname,'users')
+const users = [
+    {name:"vasya", gender:'male',age:22},
+    {name:"vanya", gender:'male',age:30},
+    {name:"yura", gender:'male',age:10},
+    {name:"ihor", gender:'male',age:40},
+    {name:"marianna", gender:'female',age:12},
+    {name:"kate", gender:'female',age:15},
+    {name:"dasha", gender:'female',age:50},
+    {name:"valya", gender:'female',age:12},
+    {name:"masha", gender:'female',age:34},
+    {name:"georgiy", gender:'male',age:87}
+]
+
+
+fs.mkdir(path.join(__dirname,'manYounger20'),(err)=>{
+    if (err){
+        console.log(err)
+    }
+})
+fs.mkdir(path.join(__dirname,'manOlder20'),(err)=>{
+    if (err){
+        console.log(err)
+    }
+})
+fs.mkdir(path.join(__dirname,'womanOlder20'),(err)=>{
+    if (err){
+        console.log(err)
+    }
+})
+fs.mkdir(path.join(__dirname,'womanYounger20'),(err)=>{
+    if (err){
+        console.log(err)
+    }
+})
+function createUser(){
+    for (user of users){
+        if ( user.gender ==='male' && user.age < 20){
+            fs.writeFile(`manYounger20/${user.name}.json`, `[{ "name":"${user.name}", "age":"${user.age}"}]` ,(err)=>{
+                console.log(err)
             })
-        })
-    })
+        }
+        if ( user.gender ==='male' && user.age > 20){
+            fs.writeFile(`manOlder20/${user.name}.json`, `[{ "name":"${user.name}", "age":"${user.age}"}]` ,(err)=>{
+                console.log(err)
+            })
+        }
+        if ( user.gender ==='female' && user.age < 20){
+            fs.writeFile(`womanYounger20/${user.name}.json`, `[{ "name":"${user.name}", "age":"${user.age}"}]` ,(err)=>{
+                console.log(err)
+            })
+        }
+        if (user.gender === 'female'&& user.age > 20){
+            fs.writeFile(`womanOlder20/${user.name}.json`, `[{ "name":"${user.name}", "age":"${user.age}"}]` ,(err)=>{
+                console.log(err)
+            })
+        }
+    }
 }
-gender('boys','girls','female')
-gender('girls','boys','male')
-
-
+createUser()
